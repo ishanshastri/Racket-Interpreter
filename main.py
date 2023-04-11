fdef3d6b04bf7acb9dda1c1e9e1af588b7dc418b
Ishaa Chopra <ishaachopra@v1040-wn-rt-a-114-149.campus-dynamic.uwaterloo.ca>
2023-04-10 20:38:13 -0400

    aintinit

--- main.py
+++ main.py
@@ -1,114 +1,10 @@
 import numpy as np
 import  math
 import ast
-'''
-def extract_elements(expr):
-    lst = expr.split()
-    operator = lst[0][1]
-    args_list = []
 
-    for arg in lst[1:]:
-        if '(' in arg:
-            end = arg.find(')') #Error handle if -1
-            print(arg)
-            args_list.append(arg[1, end])
-        else:
-            args_list.append(arg)
-
-    #args_list = lst[1:len(lst)]
-    return (operator, np.array(args_list))
-
-#intersecting splits ( and ) 
-def extract_elements2(expr):
-    opbr = expr.split('(')
-    clbr = expr.split(')')
-    #print(opbr)
-    #print(clbr)
-
-    #for e in expr:
-
-    
-def evaluate_exp(exp):
-    exp_stack = []
-    for i in range(len(exp)):
-        if exp[i]=='(':
-            exp_stack.append(evaluate_exp(exp[i: exp.find(')')]))
-            i = exp.find(')')
-        else:
-            exp_stack.append(exp[i])
-    if '(' not in exp: # If base value then just return
-        return exp
-
-    #Get operator and arguments (if not a base value)
-    #exp_tup = extract_elements(exp)
-    #op = exp_tup[0]
-    #args = exp_tup[1]
-
-    #cases
-    if op=="+":
-        func = lambda a, b: a+b
-    if op=="-":
-        func = lambda a, b: a-b
-    if op=="*":
-        func = lambda a, b: a*b
-    if op=="/": 
-        func = lambda a, b: a/b
-
-    #for v in args:
-
-def eval_expp(expr):
-    if '(' not in expr: # If base value then just return
-        return expr
-
-    exp_stack = []
-    for i in range(len(expr)):
-        if expr[i]=='(':
-            exp_stack += (eval_expp(expr[i: expr.find(')')]))
-            i = expr.find(')')
-        elif expr[i]!="" and expr[i]!=" ":
-            exp_stack.append(expr[i])
-        #print(expr[i])
-    return exp_stack
-
-def eval_arithmetic(val1, val2, op):
-    return ''
-
-def eval_exp(expr):
-    if '(' not in expr: # If base value then just return
-        return expr
-
-
-def extr_exp_2(rexp):
-    lst = expr.split()
-    operator = lst[1]
-    args_list = []
-    rest = lst[1:-1]
-    inbrac = False
-    for i in range(len(rest)):
-        print(i)
-        if rest[i] == '(':
-            inbrac = True
-            continue
-        if inbrac:
-            indof = rest.index(')')+1
-            args_list.append(' '.join(rest[i:indof]))
-            
-        elif rest[i] != ')':
-            args_list.append(rest[i])
-    return (operator, args_list)
-'''
 functions = {}#funcs
 
 #Elementry ops:
-'''
-functions['+'] = lambda a, b: a+b
-functions['-'] = lambda a, b: a-b
-functions['*'] = lambda a, b: a*b
-functions['/'] = lambda a, b: a/b
-functions['expt'] = lambda a, b: a**b
-functions['abs'] = lambda a: abs(a)
-functions['&'] = lambda a:a
-'''
 functions['+'] = lambda a : (lambda b:b+a)
 functions['-'] = lambda a: (lambda b:a-b)
 functions['*'] = lambda a: (lambda b:b*a)
@@ -129,10 +25,6 @@ def is_function(val):
 def preprocess(exp):
     #add spaces where necc
     result = []
-    #curind = exp.find('(')
-    #while curind != -1:
-        #if exp[curind+1]!=' ':
-    #return exp.split()
 
     for i in range(len(exp)-1):
         result.append(exp[i])
@@ -182,19 +74,6 @@ def eval_expr(exp):
 
     exp = extr_expr(exp)
     op = exp[0]
-
-    #cases
-    '''
-    func = lambda a:a
-    if op=='+':
-        func = lambda a, b: a+b
-    if op=='-':
-        func = lambda a, b: a-b
-    if op=='*':
-        func = lambda a, b: a*b
-    if op=='/': 
-        func = lambda a, b: a/b
-    '''
     func = functions[op]
     args = exp[1][1:]
     res = eval_expr(exp[1][0])
@@ -228,10 +107,7 @@ def run(rkt):
         print("Code Monkey")
         return
     print(eval_expr_lambda(rkt))#STUB; only for simple expressions; first extract contants, encode functions, etc.
-'''
-def extr_body(rbody):
-    return ""
-'''
+
 def get_func_def(rfunc):
     """A tuple consisting of function name (str) and params (Lst[str])"""
     rfunc = preprocess(rfunc)
@@ -251,33 +127,7 @@ def get_func_body(rfunc):#STUB
     expr = extr_expr(rbody)
     
     return expr
-#print(get_func_body("(define (f x y z) (* z (+ x y)))"))
-'''
-def repl(s, params, args):
-    print(params)
-    for i in range(len(params)):
-        s = s.replace(params[i], args[i])
 
-def repl(exp, param, arg):
-    return exp.replace(param, arg)
-
-def sub_val(expr, args):
-    """args must be in correct order, args are chars"""
-    body = get_func_body(expr)
-    params = get_func_def(expr)[1]
-    dumargs = np.array(body[1]) #dummy arguments (eg. x, y, z)
-    rep = np.vectorize(repl, excluded=[0])#str.replace)
-    #vectorize and replace all occurences of vars with respective values; then send to be evaluated!
-    
-    #print(dumargs)
-    #subbed_exp = rep(dumargs, params, args)
-    
-    print(get_raw_body(expr))
-    subbed_exp = rep(get_raw_body(expr), params, args)
-    #for e in dumargs:
-    #    e.rep(args[np.where(dumargs == e)])
-    return subbed_exp
-'''
 def repl(exp, params, args):
     for i in range(len(params)):
         exp = exp.replace(params[i], str(args[i]))
@@ -328,56 +178,11 @@ print(eval_expr_lambda("(f 2 1 1)"))
 
 #expi = lambda x: lambda y: lambda z: eval_expr_lambda(sub_val("(define (f x y z) (* z (+ x y)))", [x, y, z]))
 #print("Example: ", expi('1')('2')('2'))
-'''
-def petvalu_expr(expr):
-    expr = get_func_body(expr)
-    return expr
-
-def add_func(rfunc):
-    defn = get_func_def(rfunc)
-    lamb_expr = ""
-    for p in defn[1]:
-        lamb_expr += 'lambda ' + p + ': ' 
-    return lamb_expr#STUB
-#print(add_func("( define ( f x y z ) ( ... ) )"))
-
-def spcomm():
-    """A function desgined to generate pain"""
-    return
-'''
 
 expr = "(+ 9 0 9 (- 9 8 ) (* 2 4 (- 8 9 ) (/ 7 6)))"
 #print(petvalu_expr("(define (f x y z) (* z (+ x y)))"))
 #run(expr) #input string from file
 #print(preprocess(expr))
 
-'''
-exec('print("Yo")')
-exec("""inc = lambda x:x+1
-functions['inc'] = inc""")
-print(functions['inc'](2))
-'''
-
-'''
-print(eval_expr_lambda(expr))
-#print(extr_expr(expr))
-#print(eval_expr(expr))
-print(eval_expr_lambda("(abs -8)"))
-#print((preprocess(expr)))
-
-x = lambda a: (lambda b: a + b)
-#print(x(x(7)(8))(9))
-#print(x(3)(4))
-
-
-sus = lambda u: (lambda a: u + [a])
-lsd = []
-#print(sus(sus(lsd)('o'))('p'))
-circle = lambda a: (lambda x: math.sqrt(a**2 + x**2))
-#print(circle(3)(3))
-sphere = lambda x: (lambda y: (lambda z: math.sqrt(x**2 + y**2 + z**2)))
-#print(sphere(1)(1)(1))
-#print(sphere(sphere(1))(5))
-'''
 #fac = lambda n : 1 if n == 0 else n * fac(n-1)
 #print(fac(5))
