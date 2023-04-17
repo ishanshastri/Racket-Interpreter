3e137188d7ec7e166583f8717d50abf1dd851996
Ishaa Chopra <ishaachopra@v1040-wn-rt-a-94-218.campus-dynamic.uwaterloo.ca>
2023-04-17 18:15:55 -0400

    y-combing

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ functions['cons'] = lambda a: (lambda b: [a] + b)
 functions['append'] = lambda a: (lambda b: b + [a])
 functions['list'] = lambda a: (lambda b: (a if(isinstance(a, list)) else [a]) + [b])
 functions['first'] = lambda l: l[0]
+functions['second'] = lambda l: functions['first'](functions['rest'](l)) 
 functions['rest'] = lambda l: l[1:]
 #functions['append'] = functions['list']
 #Bool Funcs
@@ -163,24 +164,34 @@ def repl(exp, params, args):
     return exp
 
 def sub_val(expr, args):
-    """args must be in correct order, args are chars"""
+    """args must be in correct order, args are chars; expr is the raw string function defn."""
     params = get_func_def(expr)[1]
     #print(expr)
     #print(get_func_def(expr)[0])
     if get_func_def(expr)[0] in get_raw_body(expr):
         print("recursive")
     subbed_exp = repl(get_raw_body(expr), params, args)
-    print(subbed_exp)
+    #print(subbed_exp)
     return subbed_exp
 
 #print(sub_val("(define (f x y z) (* z (+ x y)))", ['1', '1', '5']))
-
+Y = lambda f: lambda x: f(Y(f))(x)
 def add_func(rfunc):
     defn = get_func_def(rfunc)
     lamb_expr = 'functions[get_func_def(rfunc)[0]] = '
+
+    lam_ext = ''
     for p in defn[1]:
         lamb_expr += 'lambda ' + p + ': ' 
+        lam_ext +=  'lambda ' + p + ': ' 
+
     args = get_func_def(rfunc)[1].copy()
+
+
+    # F = lambda fac: (lambda n: eval_expr(sub_val(n, fac)))
+    #functions['fac'] = lambda n: Y(F)(n)
+    #functions['fac'] = lambda n: Y( lambda fac: (lambda n: eval_expr(sub_val(n, fac))) )(n)
+
     #print(lamb_expr)
     #print(str(ast.literal_eval(args)))
     stringify = np.vectorize(str)
@@ -191,10 +202,26 @@ def add_func(rfunc):
         stringified_args+= a + ", "
     stringified_args = stringified_args[0:-2] 
     stringified_args += "]"
+
+    #Handle recursive functions
+    #F = 'lambda ' + get_func_def(rfunc)[0] + ':' + lam_ext + 'eval_expr_lambda(sub_val(\"' + rfunc + '\", ' +  stringified_args + '))'
+    curried_args = ''
+    for a in args:
+        curried_args += '(' + a + ')'
+    F = 'lambda ' + get_func_def(rfunc)[0] + ':' + lam_ext + get_func_def(rfunc)[0] + curried_args
+    if (get_func_def(rfunc)[0]=="fac"):#stub
+        lamb_expr += 'Y(' + F + ')'
+        for a in args:
+            lamb_expr += '(' + a + ')'
+        print(lamb_expr)
+        exec(lamb_expr)
+        return functions
+        #return 
+
     #print(stringified_args)
     #lamb_expr += 'eval_expr_lambda(sub_val(\"' + rfunc + '\", ' +  str(stringify(np.array(args)).ravel()) + '))'
     lamb_expr += 'eval_expr_lambda(sub_val(\"' + rfunc + '\", ' +  stringified_args + '))'
-    #print(lamb_expr)
+    print(lamb_expr)
     #print(rfunc, " d")
     #print(eval_expr_lambda(sub_val("(define (f x y z) (* z (+ x y)))", ['2', '1', '1'])))#make string rep of args
     #print(lamb_expr)
@@ -211,6 +238,8 @@ add_func("(define (f x y z) (* z (+ x y)))")
 print(eval_expr_lambda("(f 2 1 1)"))
 
 add_func("(define (fac n) (if (= n 1) 1 (* n (fac (- n 1)))))")
+print(eval_expr_lambda("(fac 5)"))
+
 #add_func("(define (facc n) (Y fac))")
 #print(functions['fac'])
 #print(eval_expr_lambda("(Y fac)"))
@@ -219,7 +248,7 @@ add_func("(define (fac n) (if (= n 1) 1 (* n (fac (- n 1)))))")
 #print(eval_expr_lambda("()"))
 expr = "(+ 9 0 9 (f (- (f 2 1 1) 1) 1 1) (- 9 8 ) (* 2 4 (- 8 9 ) (/ 7 6)))"
 print(eval_expr_lambda(expr))
-print(eval_expr_lambda("(list 1 3 4 (cons -1 (append 6 (list 2 4 2))))"))
+print(eval_expr_lambda("(second (list 1 3 4 (cons -1 (append 6 (list 2 4 2)))))"))
 #print(eval_expr_lambda("(true 9 8)"))
 #print(petvalu_expr("(define (f x y z) (* z (+ x y)))"))
 #run(expr) #input string from file
@@ -235,6 +264,12 @@ iff = lambda a: (lambda b: (lambda c: b if(a) else c))
 #Y = lambda f: (lambda x: f(f(x)))(lambda x: f(f(x)))
 Y = lambda f: lambda x: f(Y(f))(x)
 F = lambda f: (lambda n: 1 if(n==0) else (n*f(n-1)))
+
+# F = lambda fac: (lambda n: eval_expr(sub_val(n, fac)))
+#functions['fac'] = lambda n: Y(F)(n)
+#functions['fac'] = lambda n: Y( lambda fac: (lambda n: eval_expr(sub_val(n, fac))) )(n)
+
+    
 #print(F(Y(F))(3))
 print(F(F(F(F(F(F(F(F(F(F)))))))))(6)) #Without the y-combinator
 print(Y(F)(9)) #Using the y-combinator
